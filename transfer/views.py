8dd890004e450ab719d3c9968ae4cf57efee2264
wintercameearly <oluwanifemi.bamgbose@stu.cu.edu.ng>
2020-06-18 18:48:25 +0100

    Feature:Added logging

--- transfer/views.py
+++ transfer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Details
 from .ransfer import send
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -9,23 +12,25 @@ def index(request):
     if request.method == "POST":
         amounts, phones, providers = [], [], []
         for key, value in request.POST.items():
-    
+
             key = key.split("_")[0]
-            if key == "amount" :
+            if key == "amount":
                 amounts.append(value)
             elif key == "phone":
                 phones.append(value)
             elif key == 'provider':
                 providers.append(value)
+        logger.warning("Collecting values")
         zipped_file = list(zip(phones, providers, amounts))
         for value in zipped_file:
             data = {
                 "Code": "",
                 "Amount": value[2],
                 "PhoneNumber": value[0],
-                "SecretKey":""
+                "SecretKey": ""
 
             }
+            logger.warning("Sending " + value[2] + " to PhoneNumber:" + value[0])
             send(data)
-            
+
     return render(request, "index.html", context)
