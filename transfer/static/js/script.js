ee630435b77648c6df368c075ec2cb213e0ef3a7
NerdPraise <praiseajayi2@gmail.com>
2020-06-19 01:05:07 +0100

    Added banner notification

--- transfer/static/js/script.js
+++ transfer/static/js/script.js
@@ -1,7 +1,98 @@
 
-    var i = 1;
-      $('.add').click(function() {
-        var field = '<div><input id="fullname"name="phone" placeholder="+234 " type="text"><select id="provider" name="provider"><option> MTN </option><option> Etisalat </option><option> Glo </option><option> Airtel </option></select><input id="amount" name="amount" placeholder="Amount" type="number"><div class="clear"></div></div>';
-        $('.del').append(field);
-        i = i+1;
-      })
+var i = 1;
+$('.add').click(function () {
+  var field = '<hr><input id="phone" name="phone_' + i + '" placeholder="+234 " type="text"><select id="provider" name="provider_' + i + '"><option> Select a Provider </option><option> MTN </option><option> Etisalat </option><option> Glo </option><option> Airtel </option></select><input id="amount" name="amount_' + i + '" placeholder="Amount" type="number"><div class="clear"></div>';
+  $('.del').append(field);
+  i = i + 1;
+})
+
+var AlertBox = function(id, option) {
+  this.show = function(msg) {
+    if (msg === ''  || typeof msg === 'undefined' || msg === null) {
+      throw '"msg parameter is empty"';
+    }
+    else {
+      var alertArea = document.querySelector(id);
+      var alertBox = document.createElement('DIV');
+      var alertContent = document.createElement('DIV');
+      var alertClose = document.createElement('A');
+      var alertClass = this;
+      alertContent.classList.add('alert-content');
+      alertContent.innerText = msg;
+      alertClose.classList.add('alert-close');
+      alertClose.setAttribute('href', '#');
+      alertBox.classList.add('alert-box');
+      alertBox.appendChild(alertContent);
+      if (!option.hideCloseButton || typeof option.hideCloseButton === 'undefined') {
+        alertBox.appendChild(alertClose);
+      }
+      alertArea.appendChild(alertBox);
+      alertClose.addEventListener('click', function(event) {
+        event.preventDefault();
+        alertClass.hide(alertBox);
+      });
+      if (!option.persistent) {
+        var alertTimeout = setTimeout(function() {
+          alertClass.hide(alertBox);
+          clearTimeout(alertTimeout);
+        }, option.closeTime);
+      }
+    }
+  };
+
+  this.hide = function(alertBox) {
+    alertBox.classList.add('hide');
+    var disperseTimeout = setTimeout(function() {
+      alertBox.parentNode.removeChild(alertBox);
+      clearTimeout(disperseTimeout);
+    }, 500);
+  };
+};
+
+// Sample invoke
+var alertShowMessage = document.querySelector('#alertShowMessage');
+var alertMessageBox = document.querySelector('#alertMessageBox');
+var alertbox = new AlertBox('#alert-area', {
+  closeTime: 5000,
+  persistent: false,
+  hideCloseButton: false
+});
+var alertboxPersistent = new AlertBox('#alert-area', {
+  closeTime: 5000,
+  persistent: true,
+  hideCloseButton: false
+});
+var alertNoClose = new AlertBox('#alert-area', {
+  closeTime: 5000,
+  persistent: false,
+  hideCloseButton: true
+});
+
+
+$(".btnss").click(function() {
+  event.preventDefault()
+  let form_url = $("#form").attr("form-data-url")
+  formdata = {}
+  tot = (($("form input").length - 1) +  (($("form input").length - 1)/2))/3
+  for (i=0; i<tot; i++){
+    formdata["phone_"+ i] = $('input[name=phone_' + i +']').val()
+    formdata["amount_"+ i] = $('input[name=amount_' + i +']').val()
+    formdata["provider_"+ i] = $('select[name=provider_' + i +']').val()
+  }
+  $.ajax({
+    url: form_url,
+        type: "POST",
+        data: formdata,
+        success: function (data) {
+            if (data.error) {
+                alert(data.error)
+            }
+            if (data.success) {
+              for (i of data.success)
+                alertbox.show(i);
+                $("form")[0].reset()
+            }
+        },
+  })
+
+})
--- transfer/views.py
+++ transfer/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from .models import Details
 from .ransfer import send
 import logging
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 logger = logging.getLogger(__name__)
-
+@csrf_exempt
 def index(request):
     context = {}
+    success, fail = [], []
     if request.method == "POST":
         amounts, phones, providers = [], [], []
         for key, value in request.POST.items():
@@ -32,10 +36,16 @@ def index(request):
             response = send(data)
             response = response.decode("utf8")
             data = json.loads(response)
-                
+            
             if data["ResponseCode"] == "200":
-                context = {"success": f"Sucessfull sent to {value[0]}"}
+                success.append(f"Successfuly sent to {value[0]} ")
             else:
-                context = {"error": f"Error sending to {value[0]}"}
+                fail.append(f"Failed to send to {value[0]}")
+
+        data = {
+            "success": success,
+            "fail":fail
+        }
+        return JsonResponse(data)
 
     return render(request, "index.html", context)
